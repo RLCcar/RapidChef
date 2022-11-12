f913c02856ed0f71521b5af46c252aec4eb2a28c
Vladislava Sicicorez <washingtonv0257@uhcl.edu>
2022-11-12 00:06:42 -0600

    Added login persistance across the site master

--- Site.Master.cs
+++ Site.Master.cs
@@ -7,6 +7,7 @@ using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Windows.Forms;
+using Microsoft.SqlServer.Management.XEvent;
 using MySql.Data.MySqlClient;
 
 
@@ -23,14 +24,38 @@ namespace RapidChef
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            if (Session["userID"] != null && Session["userName"] != String.Empty)
+            {
+                TextBox1.Visible = false;
+                TextBox2.Visible = false;
+                Label1.Visible = false;
+                Label2.Visible = false;
+                Button1.Visible = false;
+                Button2.Visible = false;
+                Button3.Text = userName;
+                Button3.Visible = true;
+                Button4.Visible = true;
+            }
+         
+
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            Session ses = new Session();
             Thisemail = TextBox1.Text;
             Thispassword = TextBox2.Text;
             getdata();
+            Session["userID"] = userID;
+            Session["userName"] = userName;
+            Session["TextBox1"] = TextBox1;
+            Session["TextBox2"] = TextBox2;
+            Session["Label1"] = Label1;
+            Session["Label2"] = Label2;
+            Session["Button1"] = Button1;
+            Session["Button2"] = Button2;
+            Session["Button3"] = Button3;
+            Session["Button4"] = Button4;
             TextBox1.Visible = false;
             TextBox2.Visible = false;
             Label1.Visible = false;
@@ -40,6 +65,8 @@ namespace RapidChef
             Button3.Text = userName;
             Button3.Visible = true;
             Button4.Visible = true;
+            
+
 
 
 
@@ -82,6 +109,7 @@ namespace RapidChef
                     dr.Read();
                     userID = dr.GetInt32("userID");
                     userName = dr.GetString("firstname");
+                    
 
                     }
                  ///   MessageBox.Show(userName); test
