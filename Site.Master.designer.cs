e6f4f9692991a7c75f7e2d2f452f321bb54380d8
Eric Thibodeaux <paulthib69@yahoo.com>
2022-11-13 16:41:34 -0600

    Reworked Masterpage.

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -96,6 +96,15 @@ namespace RapidChef
         protected global::System.Web.UI.WebControls.Button Button4;
 
         /// <summary>
+        /// Menu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Menu;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
